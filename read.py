6a79a90ffc4a0499d85266a594835dbeaf9b7b67
yuki kikuchi <kikku650@gmail.com>
2018-12-05 22:20:17 +0900

    csv読み取り追加

--- read.py
+++ read.py
@@ -2,8 +2,16 @@
 # -*- coding: utf-8 -*-
 # UID取得プログラム
 from ctypes import *
-defaulrDLLpass = "./SheepSmartCard.dll"
-def read_UID(libpass=defaulrDLLpass):
+import os
+from glob import glob
+import csv
+import linecache
+from datetime import datetime
+
+defaultDLLpass = "./SheepSmartCard.dll"
+logfilerlink = "./esddata"
+target_place = [3, 5]
+def read_UID(libpass=defaultDLLpass):
     # DLLファイルをロード
     dll = windll.LoadLibrary(libpass)
     # 返り値の型を指定
@@ -21,7 +29,7 @@ def read_UID(libpass=defaulrDLLpass):
     UID_str = ''
     if(ret != 0):
         print("読み取りエラー")
-        return -1
+        return 0
     # エンディアン変換 + ASCIIコードに変換
     for i in range(8):
         data[i] = val & 0xff
@@ -29,9 +37,37 @@ def read_UID(libpass=defaulrDLLpass):
         UID_str += chr(data[i])
     return str(UID_str)
 
-def serch_UID(filename, uid):
+def get_latestfile(dirname):
+    target = os.path.join(dirname, '*')
+    files = [(f, os.path.getmtime(f)) for f in glob(target)]
+    latest_modified_file_path = sorted(files, key=lambda files: files[1])[-1]
+    return latest_modified_file_path[0]
 
+def get_result_latestESD(filename):
+    with open(filename, 'r') as input:
+        spamreader = csv.reader(input, delimiter=',', quotechar='|')
+        data = list(spamreader)
+        data_len = len(data)
+        target = data[data_len - 1]
+        # print("右足の測定値", target[target_place[0] + 1], "Ω", "左足の測定値", target[target_place[1] + 1], "[Ω]")
+        print(target)
+        if (target[target_place[0]] == "OK" and target[target_place[1]] == "OK"):
+            return 0
+    return -1
 
 if __name__ == "__main__":
-    UID = read_UID()
-    print("UID:", UID)
+    # UID = read_UID()
+    # res = get_result_latestESD()
+    while (1):
+        UID = read_UID()
+        if (UID != 0):
+            time = datetime.now()
+            break
+        sleep(0.1)
+    while (time > datetime.datetime.fromtimestamp(os.stat(get_latestfile(logfirelink)).st_mtime)):
+        continue
+    res = get_result_latestESD(get_latestfile(logfirelink))
+    if (res == 0):
+        print("success!!!")
+    else:
+        print("failed...")
